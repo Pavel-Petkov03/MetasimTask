0e8125d2f8a70c22202237e577a403c09612733b
Pavel Petkov <pavel.petkov2003@gmail.com>
2025-08-07 18:46:02 +0300

    feat(scripts) : added constants for folders for noise

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,3 +1,5 @@
 API_URL = "http://localhost:8000/"
 LLM_CHUNK_SIZE = 1000
 LLM_CHUNK_OVERLAP = 200
+TARGET_INPUT_FOLDER = "../src/text_with_noise/"
+TARGET_OUTPUT_FOLDER = "../src/text_without_noise/"
